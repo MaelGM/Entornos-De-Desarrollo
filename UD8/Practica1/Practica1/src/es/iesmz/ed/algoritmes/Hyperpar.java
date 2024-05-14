8892f0c9c48260a8db66f2e605bc000143179b2c
mael <mael.grandguillot@gmail.com>
2024-05-14 18:01:50 +0200

    Documentación del proyecto

--- UD8/Practica1/Practica1/src/es/iesmz/ed/algoritmes/Hyperpar.java
+++ UD8/Practica1/Practica1/src/es/iesmz/ed/algoritmes/Hyperpar.java
@@ -1,12 +1,28 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Esta clase es usada para comprobar si todos los dígitos de un número son par.
+ *
+ * @version 1.0
+ * @author Mael Grandguillot
+ */
 public class Hyperpar {
     private final long num;
 
+    /**
+     * Constructor de la clase Hyperpar.
+     *
+     * @param num Numero a comprobar
+     */
     public Hyperpar(long num) {
         this.num = num;
     }
 
+    /**
+     * Método que comprueba si el número es Hyperpar,<br> fragmentándolo y comprobando digito por digito
+     *
+     * @return Devuelve 'true' en caso de que si lo sea, y false en el contrario.
+     */
     public boolean esHyperPar(){
         boolean isHyperPar = true;
         char[] numbers = String.valueOf(num).toCharArray();
