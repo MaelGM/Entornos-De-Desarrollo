caa02fc417759523f6806bbf996a8373a37644b3
mael <mael.grandguillot@gmail.com>
2024-05-14 17:48:03 +0200

    Clases inicializadas

--- UD8/Practica1/Practica1/src/es/iesmz/ed/algoritmes/Hyperpar.java
+++ UD8/Practica1/Practica1/src/es/iesmz/ed/algoritmes/Hyperpar.java
@@ -8,12 +8,6 @@ public class Hyperpar {
     }
 
     public boolean esHyperPar(){
-        boolean isHyperPar = true;
-        char[] numbers = String.valueOf(num).toCharArray();
-        for (char number : numbers) {
-            if (Integer.parseInt(String.valueOf(number)) % 2 != 0) isHyperPar = false;
-        }
-
-        return isHyperPar;
+        return false;
     }
 }
