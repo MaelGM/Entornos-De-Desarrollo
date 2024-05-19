283862e2672f290a3fa09b50141f99e819583fea
mael <mael.grandguillot@gmail.com>
2024-05-19 17:55:00 +0200

    Metodo iniciado y primer test funcional

--- UD8/Practica1/src/es/iesmz/ed/algoritmes/Any.java
+++ UD8/Practica1/src/es/iesmz/ed/algoritmes/Any.java
@@ -1,4 +1,21 @@
 package es.iesmz.ed.algoritmes;
 
+import java.util.ArrayList;
+
 public class Any {
+    private final long number;
+    
+    public Any(long number) {
+        this.number = number;
+    }
+
+    public int digitsDiferents() {
+        String strNumber = String.valueOf(number);
+        ArrayList<Character> digitos = new ArrayList<>();
+        for (int i = 0; i < strNumber.length(); i++) {
+            if (!digitos.contains(strNumber.charAt(i))) digitos.add(strNumber.charAt(i));
+        }
+
+        return digitos.size();
+    }
 }
--- UD8/Practica1/src/es/iesmz/ed/algoritmes/AnyTest.java
+++ UD8/Practica1/src/es/iesmz/ed/algoritmes/AnyTest.java
@@ -1,6 +1,7 @@
 package es.iesmz.ed.algoritmes;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
