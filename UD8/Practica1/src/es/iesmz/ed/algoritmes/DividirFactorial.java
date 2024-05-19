081c29f74dcba1858a58c4f6987ad3a13e5fd815
mael <mael.grandguillot@gmail.com>
2024-05-19 18:19:32 +0200

    Clases DivididirFactorial iniciadas

---
+++ UD8/Practica1/src/es/iesmz/ed/algoritmes/DividirFactorial.java
@@ -0,0 +1,16 @@
+package es.iesmz.ed.algoritmes;
+
+public class DividirFactorial {
+    private final int numerador;
+    private final int denominador;
+    public DividirFactorial(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public float divisio() {
+
+        return 1;
+
+    }
+}
---
+++ UD8/Practica1/src/es/iesmz/ed/algoritmes/DividirFactorialTest.java
@@ -0,0 +1,14 @@
+package es.iesmz.ed.algoritmes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DividirFactorialTest {
+    @Test
+    void test1(){
+        DividirFactorial divFac = new DividirFactorial(5, 3);
+        assertEquals(20, divFac.divisio());
+    }
+
+}
