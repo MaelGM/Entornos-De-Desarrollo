8b98801369ed3be4d68b80215d34dc1eba645c32
mael <mael.grandguillot@gmail.com>
2024-05-19 17:42:52 +0200

    Clase Any & AnyTest iniciadas

---
+++ UD8/Practica1/Practica1/src/es/iesmz/ed/algoritmes/AnyTest.java
@@ -0,0 +1,14 @@
+package es.iesmz.ed.algoritmes;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnyTest {
+    @Test
+    void test1(){
+        Any x = new Any(1425);
+        assertEquals(4, x.digitsDiferents());
+    }
+
+}
