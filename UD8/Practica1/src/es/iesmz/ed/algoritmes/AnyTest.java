2072d731c3f36402a0c52dc39fb6642f5c0d0afb
mael <mael.grandguillot@gmail.com>
2024-05-19 17:58:09 +0200

    Tests realizados y casos comprobados

--- UD8/Practica1/src/es/iesmz/ed/algoritmes/AnyTest.java
+++ UD8/Practica1/src/es/iesmz/ed/algoritmes/AnyTest.java
@@ -7,9 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AnyTest {
     @Test
-    void test1(){
+    void test_NumerosDiferentes(){
         Any x = new Any(1425);
         assertEquals(4, x.digitsDiferents());
+    }
+
+    @Test
+    void test_NumeroRepetido(){
+        Any x = new Any(1111);
+        assertEquals(1, x.digitsDiferents());
+    }
+
+    @Test
+    void test_NumerosMezclados(){
+        Any x = new Any(152844599);
+        assertEquals(6, x.digitsDiferents());
     }
 
 }
