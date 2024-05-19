1cee16a793cc377610a23175f9fa30a31219878c
mael <mael.grandguillot@gmail.com>
2024-05-19 18:31:31 +0200

    Tests finalizador y parametrizados

--- UD8/Practica1/src/es/iesmz/ed/algoritmes/DividirFactorialTest.java
+++ UD8/Practica1/src/es/iesmz/ed/algoritmes/DividirFactorialTest.java
@@ -1,14 +1,21 @@
 package es.iesmz.ed.algoritmes;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DividirFactorialTest {
-    @Test
-    void test1(){
-        DividirFactorial divFac = new DividirFactorial(5, 3);
-        assertEquals(20, divFac.divisio(), 0.001);
+    @ParameterizedTest
+    @CsvSource({
+            "5, 3, 20",
+            "10, 7, 720",
+            "3, 5, 0.05",
+            "7, 7, 1"
+    })
+    void test1(String num, String den, String expected){
+        DividirFactorial divFac = new DividirFactorial(Integer.parseInt(num), Integer.parseInt(den));
+        assertEquals(Float.parseFloat(expected), divFac.divisio(), 0.001);
     }
 
 }
