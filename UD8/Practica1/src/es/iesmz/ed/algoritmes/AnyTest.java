7cfab91467b70a5f06a909c35458615582b94efe
mael <mael.grandguillot@gmail.com>
2024-05-19 18:04:18 +0200

    Tests refactorizados

--- UD8/Practica1/src/es/iesmz/ed/algoritmes/AnyTest.java
+++ UD8/Practica1/src/es/iesmz/ed/algoritmes/AnyTest.java
@@ -1,27 +1,22 @@
 package es.iesmz.ed.algoritmes;
 
-
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnyTest {
-    @Test
-    void test_NumerosDiferentes(){
-        Any x = new Any(1425);
-        assertEquals(4, x.digitsDiferents());
-    }
 
-    @Test
-    void test_NumeroRepetido(){
-        Any x = new Any(1111);
-        assertEquals(1, x.digitsDiferents());
-    }
-
-    @Test
-    void test_NumerosMezclados(){
-        Any x = new Any(152844599);
-        assertEquals(6, x.digitsDiferents());
+    @ParameterizedTest
+    @CsvSource({
+            "1425, 4",
+            "1115, 2",
+            "152844599, 6",
+            "1542847621587, 7"
+    })
+    void test_NumerosDiferentes(String number, String expected){
+        Any x = new Any(Long.parseLong(number));
+        assertEquals(Integer.parseInt(expected), x.digitsDiferents());
     }
 
 }
